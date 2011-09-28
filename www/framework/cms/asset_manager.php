185c383ae096a660f65e7c952264833b0f77519d
Lion Vollnhals <lion.vollnhals@googlemail.com>
2011-09-28 13:20:47 +0200

    extract original_filename, processed_filename, parent_id, filetype, width, height and created_at automatically when calling create

--- www/framework/cms/asset_manager.php
+++ www/framework/cms/asset_manager.php
@@ -106,17 +106,26 @@ class asset_manager {
     }
 
     // {{{ create
-    // dir is of form "foo/bar"
-    // split dir on "/"
-    // each part is automatically a tag
-    // additional tags may be supplied
-    // find matching
-    //
-    public function create($original_file, $original_filename, $processed_filename, $xml_path, $filetype = null, $size = null, $created_at = null, $page_id = null, $additional_tags = array()) {
+    /*
+     * creates a new asset
+     *
+     * @param       $original_file (string)     path to file on disk
+     * @param       $xml_path (string)          path in xml tree to structure assets. for example "/photos/new/good".
+     *                                          path is split on "/". each part will automatically become a tag.
+     * @param       $additional tags            additional tags for this asset
+     */
+    public function create($original_file, $xml_path, $page_id = null, $additional_tags = array()) {
+        $path_parts = pathinfo($original_file);
+        $original_filename = $path_parts["basename"];
+        $filetype = $path_parts["extension"];
+        $processed_filename = self::process_filename($path_parts["filename"]);
+        list($width, $height) = getimagesize($original_file);
+        $created_at = time();
+
         $parent_id = $this->create_xml_dirs($xml_path);
         $path_tags = array_filter(explode("/", $xml_path));
 
-        return $this->basic_create($original_file, $original_filename, $processed_filename, $parent_id, -1, $filetype, $size, $created_at, $page_id, array_merge($additional_tags, $path_tags));
+        return $this->basic_create($original_file, $original_filename, $processed_filename, $parent_id, -1, $filetype, $width, $height, $created_at, $page_id, array_merge($additional_tags, $path_tags));
     }
     // }}}
 
@@ -237,6 +246,26 @@ class asset_manager {
 
     static private function full_asset_path($timestamp) {
         return DEPAGE_PATH . "/" . self::PARTIAL_ASSET_PATH . "/" . date("Y/m", $timestamp);
+    }
+
+    static private function process_filename($original_filename) {
+        // inspired by http://neo22s.com/slug/
+        $accents = array('À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'ß', 'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'Ā', 'ā', 'Ă', 'ă', 'Ą', 'ą', 'Ć', 'ć', 'Ĉ', 'ĉ', 'Ċ', 'ċ', 'Č', 'č', 'Ď', 'ď', 'Đ', 'đ', 'Ē', 'ē', 'Ĕ', 'ĕ', 'Ė', 'ė', 'Ę', 'ę', 'Ě', 'ě', 'Ĝ', 'ĝ', 'Ğ', 'ğ', 'Ġ', 'ġ', 'Ģ', 'ģ', 'Ĥ', 'ĥ', 'Ħ', 'ħ', 'Ĩ', 'ĩ', 'Ī', 'ī', 'Ĭ', 'ĭ', 'Į', 'į', 'İ', 'ı', 'Ĳ', 'ĳ', 'Ĵ', 'ĵ', 'Ķ', 'ķ', 'Ĺ', 'ĺ', 'Ļ', 'ļ', 'Ľ', 'ľ', 'Ŀ', 'ŀ', 'Ł', 'ł', 'Ń', 'ń', 'Ņ', 'ņ', 'Ň', 'ň', 'ŉ', 'Ō', 'ō', 'Ŏ', 'ŏ', 'Ő', 'ő', 'Œ', 'œ', 'Ŕ', 'ŕ', 'Ŗ', 'ŗ', 'Ř', 'ř', 'Ś', 'ś', 'Ŝ', 'ŝ', 'Ş', 'ş', 'Š', 'š', 'Ţ', 'ţ', 'Ť', 'ť', 'Ŧ', 'ŧ', 'Ũ', 'ũ', 'Ū', 'ū', 'Ŭ', 'ŭ', 'Ů', 'ů', 'Ű', 'ű', 'Ų', 'ų', 'Ŵ', 'ŵ', 'Ŷ', 'ŷ', 'Ÿ', 'Ź', 'ź', 'Ż', 'ż', 'Ž', 'ž', 'ſ', 'ƒ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ǎ', 'ǎ', 'Ǐ', 'ǐ', 'Ǒ', 'ǒ', 'Ǔ', 'ǔ', 'Ǖ', 'ǖ', 'Ǘ', 'ǘ', 'Ǚ', 'ǚ', 'Ǜ', 'ǜ', 'Ǻ', 'ǻ', 'Ǽ', 'ǽ', 'Ǿ', 'ǿ');
+        $repl = array('A', 'A', 'A', 'A', 'A', 'A', 'AE', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'D', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 's', 'a', 'a', 'a', 'a', 'a', 'a', 'ae', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'n', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', 'A', 'a', 'A', 'a', 'A', 'a', 'C', 'c', 'C', 'c', 'C', 'c', 'C', 'c', 'D', 'd', 'D', 'd', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'G', 'g', 'G', 'g', 'G', 'g', 'G', 'g', 'H', 'h', 'H', 'h', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'IJ', 'ij', 'J', 'j', 'K', 'k', 'L', 'l', 'L', 'l', 'L', 'l', 'L', 'l', 'l', 'l', 'N', 'n', 'N', 'n', 'N', 'n', 'n', 'O', 'o', 'O', 'o', 'O', 'o', 'OE', 'oe', 'R', 'r', 'R', 'r', 'R', 'r', 'S', 's', 'S', 's', 'S', 's', 'S', 's', 'T', 't', 'T', 't', 'T', 't', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'W', 'w', 'Y', 'y', 'Y', 'Z', 'z', 'Z', 'z', 'Z', 'z', 's', 'f', 'O', 'o', 'U', 'u', 'A', 'a', 'I', 'i', 'O', 'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'A', 'a', 'AE', 'ae', 'O', 'o');
+        $processed_filename = str_replace($accents, $repl, $original_filename);
+
+        $processed_filename = strtolower(trim($processed_filename));
+
+        // adding - for spaces and union characters
+        $find = array(' ', '&', '\r\n', '\n', '+', ',');
+        $processed_filename = str_replace ($find, '-', $processed_filename);
+
+        // delete and replace rest of special chars
+        $find = array('/[^a-z0-9\-<>]/', '/[\-]+/', '/<[^>]*>/');
+        $repl = array('', '-', '');
+        $processed_filename = preg_replace ($find, $repl, $processed_filename);
+
+        return $processed_filename;
     }
 }
 
